1d8420894427c3ec715425f527b0f8cdb7765504
Divya S. Moka <divyashrimoka20@gmail.com>
2022-11-16 13:01:24 -0600

    made required changes

--- controllers/burger.js
+++ controllers/burger.js
@@ -54,11 +54,18 @@ exports.burger_create_post = async function(req, res) {
 //exports.burger_detail = function(req, res) {
 //res.send('NOT IMPLEMENTED: Burger detail: ' + req.params.id);
 //};
-
-// Handle Burger delete form on DELETE.
-exports.burger_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: Burger delete DELETE ' + req.params.id);
-};
+// Handle burger delete on DELETE.
+exports.burger_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Burger.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
 // Handle Burger update form on PUT.
 exports.burger_update_put = async function(req, res) {
     console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
@@ -86,5 +93,56 @@ exports.burger_detail = async function(req, res) {
     } catch (error) {
     res.status(500)
     res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+// Handle a show one view with id specified by query
+exports.burger_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Burger.findById( req.query.id)
+    res.render('burgerdetail',
+   { title: 'Burger Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+// Handle building the view for creating a burger.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.burger_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('burgercreate', { title: 'Burger Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+// Handle building the view for updating a burger.
+// query provides the id
+exports.burger_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Burger.findById(req.query.id)
+    res.render('burgerupdate', { title: 'Burger Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+// Handle a delete one view with id from query
+exports.burger_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Burger.findById(req.query.id)
+    res.render('burgerdelete', { title: 'Burger Delete', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
    };
--- routes/burger.js
+++ routes/burger.js
@@ -4,5 +4,13 @@ var router = express.Router();
 /* GET burger */
 router.get('/', burger_controlers.burger_view_all_Page );
 module.exports = router;
-// GET request for one costume.
+// GET request for one burger.
 router.get('/burger/:id', burger_controlers.burger_detail);
+/* GET detail burger page */
+router.get('/detail', burger_controlers.burger_view_one_Page);
+/* GET create burger page */
+router.get('/create', burger_controlers.burger_create_Page);
+/* GET create update page */
+router.get('/update', burger_controlers.burger_update_Page);
+/* GET delete burger page */
+router.get('/delete', burger_controlers.burger_delete_Page);
