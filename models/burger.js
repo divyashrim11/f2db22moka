7bf45bb03716024c192b1e2d02c9439cbc503b20
Divya S. Moka <divyashrimoka20@gmail.com>
2022-11-30 19:03:36 -0600

    made required changes

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const burgerSchema = mongoose.Schema({
-BurgerType: String,
-BurgerSize: String,
-BurgerCost: Number
+BurgerType: {type: String,required: [true, 'Burger type cannot be empty']},
+BurgerSize: {type: String,required: [true, 'Burger size cannot be empty']},
+BurgerCost: {type: Number,required: [true, 'Burger cost cannot be empty']}
 })
 module.exports = mongoose.model("burger",burgerSchema)
--- routes/burger.js
+++ routes/burger.js
@@ -1,16 +1,26 @@
 var express = require('express');
 const burger_controlers= require('../controllers/burger');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET burger */
 router.get('/', burger_controlers.burger_view_all_Page );
 module.exports = router;
 // GET request for one burger.
 router.get('/burger/:id', burger_controlers.burger_detail);
 /* GET detail burger page */
-router.get('/detail', burger_controlers.burger_view_one_Page);
+router.get('/detail', secured, burger_controlers.burger_view_one_Page);
 /* GET create burger page */
-router.get('/create', burger_controlers.burger_create_Page);
+router.get('/create', secured, burger_controlers.burger_create_Page);
 /* GET create update page */
-router.get('/update', burger_controlers.burger_update_Page);
+router.get('/update',secured, burger_controlers.burger_update_Page);
 /* GET delete burger page */
-router.get('/delete', burger_controlers.burger_delete_Page);
+router.get('/delete',secured, burger_controlers.burger_delete_Page);
